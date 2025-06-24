c2b06bd90bf6c1fc135e779cf33fa723e9a6016b
EricBarzman <ericbarzman@gmail.com>
2025-06-24 18:08:56 +0200

    search algo

--- src/utils/searchMovies.ts
+++ src/utils/searchMovies.ts
@@ -1,8 +1,21 @@
 import { IMovie } from "../types/movie.type";
 
+/**
+ * L'algorithme de recherche de ClassicMovies.
+ * Il remplace une recherche SQL,
+ * la BDD Firebase NoSQL ne permettant pas une recherche efficace.
+ * 
+ * @param input La chaîne de recherche
+ * @param movies Les films parmi lesquels on cherche
+ * @returns 
+ */
 function searchMoviesFromInput(input: string, movies: IMovie[]): IMovie[] {
   return movies.filter(movie =>
-    movie.title.toLowerCase().includes(input)
+    // Le titre contient-il l'input ?
+    movie.title.toLowerCase().includes(input.toLowerCase()) ||
+    // Le nom réal contient-il l'input ?
+    movie.director.lastName.toLowerCase().includes(input.toLowerCase()) ||
+    movie.director.firstName.toLowerCase().includes(input.toLowerCase())
   )
 }
 
